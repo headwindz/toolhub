e4b2c980e3ae9dd26983f8433ca7e630e6a87b93
headwindz <zhengjianhua.michael@gmail.com>
2026-01-30 23:40:43 +0800

    feat: add pwa support

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+import withPWA from 'next-pwa'
+
+const nextConfig = withPWA({
   typescript: {
     ignoreBuildErrors: true,
   },
   images: {
     unoptimized: true,
   },
- 
-}
+  pwa: {
+    dest: 'public',
+    register: true,
+    skipWaiting: true,
+  },
+})
 
 export default nextConfig
